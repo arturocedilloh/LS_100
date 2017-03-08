2e6a3c362f7d6203fbeb1df4c9046c5e569f102c
Arturo Cedillo <arturo.cedillo.h@live.com>
2017-03-08 22:55:09 +1100

    Updated solution 3 and added 5 and 6 to complete the section

--- intro_to_prog/basics/3_solution.rb
+++ intro_to_prog/basics/3_solution.rb
@@ -4,3 +4,15 @@ puts movies[:"Star Wars"]
 puts movies[:"Star Trek"]
 puts movies[:"Jaws"]
 puts movies[:"Batman"]
+
+dates = [ movies[:"Hulk"], movies[:"Star Wars"], movies[:"Star Trek"],
+          movies[:"Jaws"], movies[:"Batman"] ]
+puts dates
+
+dates = [1975, 2004, 2013, 2001, 1981]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
