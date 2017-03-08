f5ec3b181a085d823096006f9380ec6174f77812
Arturo Cedillo <arturo.cedillo.h@live.com>
2017-03-08 22:45:23 +1100

    First save

---
+++ intro_to_prog/basics/3_solution.rb
@@ -0,0 +1,6 @@
+movies = {:"Hulk" => "1975", :"Star Wars" => "2004", :"Star Trek" => "2013", :"Jaws" => "2001", :"Batman" => "1981"}
+puts movies[:"Hulk"]
+puts movies[:"Star Wars"]
+puts movies[:"Star Trek"]
+puts movies[:"Jaws"]
+puts movies[:"Batman"]
